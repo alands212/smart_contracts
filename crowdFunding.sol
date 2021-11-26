2f616aed4c2268f488d75af527551273c39c24a9
Alan Martín Da Silva <51641125+alands212@users.noreply.github.com>
2021-11-26 11:27:12 -0300

    news require

--- crowdFunding.sol
+++ crowdFunding.sol
@@ -7,7 +7,7 @@ contract CrowdFunding{
     string public name;
     string public description;
     address payable public author;
-    string  public  state = 'Opened';
+    uint  public  state = 1;
     uint public funds;
     uint public fundraisingGoal;
 
@@ -18,7 +18,7 @@ contract CrowdFunding{
 
     event ProjectStateChanged(
         string id,
-        string state
+        uint state
     );
 
     constructor(string memory _id, string memory _name, string memory _description, uint _fundraisingGoal){
@@ -46,12 +46,19 @@ contract CrowdFunding{
     }
 
     function fundProject() public payable isNotAuthor{
+
+        require(state != 1, "El projecto ya no recibe fondos.");
+        require(msg.value > 0, "Los fondos invertidos deben ser mayor a 0.");
+
         author.transfer(msg.value);
         funds += msg.value;
         emit ProjectFunded(id, msg.value);
     }
 
-    function changeProjectState(string calldata newState) public isAuthor{
+    function changeProjectState(uint newState) public isAuthor{
+
+        require(state != newState, "El projecto ya se encuentra en ese estado.");
+
         state = newState;
         emit ProjectStateChanged(id, newState);
     }
